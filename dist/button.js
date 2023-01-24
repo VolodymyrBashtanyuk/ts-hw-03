21fba14b79fb3a72d4f0026dfb4bc8e2631efe23
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2023-01-24 21:14:45 +0200

    complete hw-03

---
+++ dist/button.js
@@ -0,0 +1,10 @@
+"use strict";
+const button = document.querySelector('.buttonId');
+let age;
+age = 20;
+age = 45;
+age = 43;
+button.addEventListener('click', () => {
+    console.log(age);
+});
+//# sourceMappingURL=button.js.map
